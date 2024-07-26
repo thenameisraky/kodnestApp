4d0689eadcb16a27c1eaf871d915d97924293cef
thenameisraky <mallemrakeshvrk369@gmail.com>
2024-07-26 16:07:49 +0530

    app modify

--- kodnest/src/App.js
+++ kodnest/src/App.js
@@ -1,9 +1,10 @@
+import Assessment from "./Assessment";
 import Attendance from "./Attendance";
 import Classes from "./Classes";
 function App() {
   return (
     <>
-    <h1>Kodnest  App <Classes />  <Attendance /></h1>
+    <h1>Kodnest  App <Classes />  <Attendance />  </h1>
     </>
   );
 }
